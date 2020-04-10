7b89dbad08497429994ae8f76af0151169cd2a4d
snehalp1003 <patel.sneha@husky.neu.edu>
2020-04-10 16:24:50 -0400

    Committing ecosystem model

---
+++ Blood Safety and Availability/src/Business/Role/Role.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Snehal
+ */
+public abstract class Role {
+    public enum RoleType{
+        Admin("Admin"),
+        Doctor("Doctor"),
+        LabAssistant("Lab Assistant");
+        
+        private String value;
+        private RoleType(String value){
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        @Override
+        public String toString() {
+            return value;
+        }
+    }
+    
+    public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+            UserAccount account, 
+            Organization organization, 
+            Enterprise enterprise, 
+            EcoSystem business);
+
+    @Override
+    public String toString() {
+        return this.getClass().getName();
+    }
+}
