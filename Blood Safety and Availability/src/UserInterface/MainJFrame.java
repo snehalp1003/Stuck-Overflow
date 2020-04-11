3e835607b3a260fbb5ed6364276c321fe08a11f9
snehalp1003 <patel.sneha@husky.neu.edu>
2020-04-11 12:27:20 -0400

    Correcting imports

--- Blood Safety and Availability/src/UserInterface/MainJFrame.java
+++ Blood Safety and Availability/src/UserInterface/MainJFrame.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package userinterface;
+package UserInterface;
 
 import Business.EcoSystem;
 import Business.DB4OUtil.DB4OUtil;
