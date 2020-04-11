f742d51b9f16896bc69096dbfd24c88ce0c62452
snehalp1003 <patel.sneha@husky.neu.edu>
2020-04-11 11:20:49 -0400

    Creating model for Blood Bank employee

--- Blood Safety and Availability/src/Business/BloodBankEmployee/BloodBankEmployeeDirectory.java
+++ Blood Safety and Availability/src/Business/BloodBankEmployee/BloodBankEmployeeDirectory.java
@@ -5,10 +5,27 @@
  */
 package Business.BloodBankEmployee;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Snehal
  */
 public class BloodBankEmployeeDirectory {
+    private ArrayList<BloodBankEmployee> bloodBankEmployeeList;
+
+    public BloodBankEmployeeDirectory() {
+        bloodBankEmployeeList = new ArrayList();
+    }
+
+    public ArrayList<BloodBankEmployee> getBloodBankEmployeeList() {
+        return bloodBankEmployeeList;
+    }    
     
+    public BloodBankEmployee createBloodBankEmployee(String name){
+        BloodBankEmployee employee = new BloodBankEmployee();
+        employee.setName(name);
+        bloodBankEmployeeList.add(employee);
+        return employee;
+    }
 }
